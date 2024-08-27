fa955437d5c5134539c516c86e602af1bb07bcad
bcmmbaga <bethuelmbaga12@gmail.com>
2024-08-27 23:09:52 +0300

    chore: update target descriptions Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>

--- pkg/types/targets.go
+++ pkg/types/targets.go
@@ -24,45 +24,50 @@ func GetTargetManifest() *provider.ProviderTargetManifest {
 		"Region": provider.ProviderTargetProperty{
 			Type:         provider.ProviderTargetPropertyTypeString,
 			DefaultValue: "us-east-1",
-			Description:  "The geographic area where AWS resourced are hosted. Default is us-east-1.",
-		},
-		"Image Id": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
-			DefaultValue: "ami-04a81a99f5ec58529",
-			Description: "The ID of the Amazon Machine Image (AMI) to launch an instance. Default is ami-04a81a99f5ec58529. " +
-				"How to find AMI that meets your needs  https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html",
+			Description: "The geographic area where AWS resourced are hosted. Default is us-east-1.\n" +
+				"List of available regions:\nhttps://docs.aws.amazon.com/general/latest/gr/rande.html",
 		},
 		"Instance Type": provider.ProviderTargetProperty{
 			Type:         provider.ProviderTargetPropertyTypeString,
 			DefaultValue: "t2.micro",
-			Description: "The type of instance to launch. Default is t2.micro. List of available instance types " +
+			Description: "The type of instance to launch. Default is t2.micro.\nList of available instance types:\n" +
 				"https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-types.html#AvailableInstanceTypes",
+		},
+		"Image Id": provider.ProviderTargetProperty{
+			Type:         provider.ProviderTargetPropertyTypeString,
+			DefaultValue: "ami-04a81a99f5ec58529",
+			Description: "The ID of the Amazon Machine Image (AMI) to launch an instance. Default is ami-04a81a99f5ec58529.\n" +
+				"How to find AMI that meets your needs:\nhttps://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html",
 		},
 		"Device Name": provider.ProviderTargetProperty{
 			Type:         provider.ProviderTargetPropertyTypeString,
 			DefaultValue: "/dev/sda1",
-			Description:  "The device name for the volume. This is typically the root device name for specified AMI.",
+			Description: "The device name for the volume. This is typically the root device name for specified AMI.\n" +
+				"List of device names:\nhttps://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html",
 		},
 		"Volume Size": provider.ProviderTargetProperty{
 			Type:         provider.ProviderTargetPropertyTypeInt,
 			DefaultValue: "20",
-			Description:  "The size of the instance volume, in GB. Default is 20 GB. It is recommended that the disk size should be more than 20 GB.",
+			Description: "The size of the instance volume, in GB. Default is 20 GB.It is recommended that the disk size should be more than 20 GB.\n" +
+				"List of volume size limits:\nhttps://docs.aws.amazon.com/AWSEC2/latest/UserGuide/volume_limits.html",
 		},
 		"Volume Type": provider.ProviderTargetProperty{
 			Type:         provider.ProviderTargetPropertyTypeString,
 			DefaultValue: "gp3",
-			Description: "The type of volume. Default is gp3. List of available volume types " +
-				"https://docs.aws.amazon.com/ebs/latest/userguide/ebs-volume-types.html",
+			Description: "The type of volume. Default is gp3.\n" +
+				"List of available volume types:\nhttps://docs.aws.amazon.com/ebs/latest/userguide/ebs-volume-types.html",
 		},
 		"Access Key Id": provider.ProviderTargetProperty{
 			Type:        provider.ProviderTargetPropertyTypeString,
 			InputMasked: true,
-			Description: "If empty, it will be fetched from the AWS_ACCESS_KEY_ID environment variable.",
+			Description: "Find this in the AWS Console under \"My Security Credentials\"\nhttps://aws.amazon.com/premiumsupport/knowledge-center/manage-access-keys/\n" +
+				"Leave blank if you've set the AWS_ACCESS_KEY_ID environment variable, or enter your Id here.",
 		},
 		"Secret Access Key": provider.ProviderTargetProperty{
 			Type:        provider.ProviderTargetPropertyTypeString,
 			InputMasked: true,
-			Description: "If empty, it will be fetched from the AWS_SECRET_ACCESS_KEY environment variable.",
+			Description: "Find this in the AWS Console under \"My Security Credentials\"\nhttps://aws.amazon.com/premiumsupport/knowledge-center/manage-access-keys/\n" +
+				"Leave blank if you've set the AWS_SECRET_ACCESS_KEY environment variable, or enter your key here.",
 		},
 	}
 }
