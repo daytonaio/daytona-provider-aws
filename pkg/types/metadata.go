620e30fffab6b1664f4da85f77d54fa3ece63411
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-01-24 12:08:47 +0100

    feat: workspace management refactor (#17)
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>
    Co-authored-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- pkg/types/metadata.go
+++ pkg/types/metadata.go
@@ -1,6 +1,6 @@
 package types
 
-type WorkspaceMetadata struct {
+type TargetMetadata struct {
 	InstanceId string
 	Tags       map[string]string
 	IsRunning  bool
--- pkg/types/targets.go
+++ pkg/types/targets.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/daytonaio/daytona/pkg/provider"
+	"github.com/daytonaio/daytona/pkg/models"
 )
 
 type TargetOptions struct {
@@ -19,53 +19,53 @@ type TargetOptions struct {
 	SecretAccessKey string `json:"Secret Access Key"`
 }
 
-func GetTargetManifest() *provider.ProviderTargetManifest {
-	return &provider.ProviderTargetManifest{
-		"Region": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
+func GetTargetConfigManifest() *models.TargetConfigManifest {
+	return &models.TargetConfigManifest{
+		"Region": models.TargetConfigProperty{
+			Type:         models.TargetConfigPropertyTypeString,
 			DefaultValue: "us-east-1",
 			Description: "The geographic area where AWS resourced are hosted. List of available regions:\n" +
 				"https://docs.aws.amazon.com/general/latest/gr/rande.html\n" +
 				"Leave blank if you've set the AWS_DEFAULT_REGION environment variable, or enter your region here.",
 		},
-		"Instance Type": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
+		"Instance Type": models.TargetConfigProperty{
+			Type:         models.TargetConfigPropertyTypeString,
 			DefaultValue: "t2.micro",
 			Description: "The type of instance to launch. Default is t2.micro.\nList of available instance types:\n" +
 				"https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-types.html#AvailableInstanceTypes",
 		},
-		"Image Id": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
+		"Image Id": models.TargetConfigProperty{
+			Type:         models.TargetConfigPropertyTypeString,
 			DefaultValue: "ami-04a81a99f5ec58529",
 			Description: "The ID of the Amazon Machine Image (AMI) to launch an instance. Default is ami-04a81a99f5ec58529.\n" +
 				"How to find AMI that meets your needs:\nhttps://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html",
 		},
-		"Device Name": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
+		"Device Name": models.TargetConfigProperty{
+			Type:         models.TargetConfigPropertyTypeString,
 			DefaultValue: "/dev/sda1",
 			Description: "The device name for the volume. This is typically the root device name for specified AMI.\n" +
 				"List of device names:\nhttps://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html",
 		},
-		"Volume Size": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeInt,
+		"Volume Size": models.TargetConfigProperty{
+			Type:         models.TargetConfigPropertyTypeInt,
 			DefaultValue: "20",
 			Description: "The size of the instance volume, in GB. Default is 20 GB.It is recommended that the disk size should be more than 20 GB.\n" +
 				"List of volume size limits:\nhttps://docs.aws.amazon.com/AWSEC2/latest/UserGuide/volume_limits.html",
 		},
-		"Volume Type": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
+		"Volume Type": models.TargetConfigProperty{
+			Type:         models.TargetConfigPropertyTypeString,
 			DefaultValue: "gp3",
 			Description: "The type of volume. Default is gp3.\n" +
 				"List of available volume types:\nhttps://docs.aws.amazon.com/ebs/latest/userguide/ebs-volume-types.html",
 		},
-		"Access Key Id": provider.ProviderTargetProperty{
-			Type:        provider.ProviderTargetPropertyTypeString,
+		"Access Key Id": models.TargetConfigProperty{
+			Type:        models.TargetConfigPropertyTypeString,
 			InputMasked: true,
 			Description: "Find this in the AWS Console under \"My Security Credentials\"\nhttps://aws.amazon.com/premiumsupport/knowledge-center/manage-access-keys/\n" +
 				"Leave blank if you've set the AWS_ACCESS_KEY_ID environment variable, or enter your Id here.",
 		},
-		"Secret Access Key": provider.ProviderTargetProperty{
-			Type:        provider.ProviderTargetPropertyTypeString,
+		"Secret Access Key": models.TargetConfigProperty{
+			Type:        models.TargetConfigPropertyTypeString,
 			InputMasked: true,
 			Description: "Find this in the AWS Console under \"My Security Credentials\"\nhttps://aws.amazon.com/premiumsupport/knowledge-center/manage-access-keys/\n" +
 				"Leave blank if you've set the AWS_SECRET_ACCESS_KEY environment variable, or enter your key here.",
